0886df2b28981801a731223091b27e53deba2f54
Tyler <tharig0327@gmail.com>
2026-06-26 07:39:31 -0400

    Fix bulk import: don't save to disk mid-transaction in sql.js wrapper

--- production-tracker/src/db.js
+++ production-tracker/src/db.js
@@ -4,12 +4,11 @@ const fs = require('fs');
 
 const DB_PATH = path.join(__dirname, '../db/production.db');
 
-// Ensure db directory exists
 fs.mkdirSync(path.dirname(DB_PATH), { recursive: true });
 
-// Wrapper that mimics better-sqlite3's synchronous API
 class SqlJsWrapper {
   constructor(sqlJs) {
+    this._inTransaction = false;
     if (fs.existsSync(DB_PATH)) {
       this._db = new sqlJs.Database(fs.readFileSync(DB_PATH));
     } else {
@@ -23,7 +22,7 @@ class SqlJsWrapper {
 
   exec(sql) {
     this._db.run(sql);
-    this._save();
+    if (!this._inTransaction) this._save();
     return this;
   }
 
@@ -34,8 +33,9 @@ class SqlJsWrapper {
         const stmt = self._db.prepare(sql);
         stmt.run(args);
         stmt.free();
-        const rowid = self._db.exec('SELECT last_insert_rowid()')[0].values[0][0];
-        self._save();
+        const rowidResult = self._db.exec('SELECT last_insert_rowid()');
+        const rowid = rowidResult.length ? rowidResult[0].values[0][0] : null;
+        if (!self._inTransaction) self._save();
         return { lastInsertRowid: rowid, changes: 1 };
       },
       get(...args) {
@@ -59,14 +59,17 @@ class SqlJsWrapper {
   transaction(fn) {
     const self = this;
     return function(...args) {
+      self._inTransaction = true;
       self._db.run('BEGIN');
       try {
         const result = fn(...args);
         self._db.run('COMMIT');
+        self._inTransaction = false;
         self._save();
         return result;
       } catch (e) {
         self._db.run('ROLLBACK');
+        self._inTransaction = false;
         throw e;
       }
     };
